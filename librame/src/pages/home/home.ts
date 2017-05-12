dc6f08c04610d48d202097d27673a23ce9105481
Gemma <gemma.johnson@hotmail.com>
2017-05-12 23:55:47 +1200

    Display all books on Home.html

--- librame/src/pages/home/home.ts
+++ librame/src/pages/home/home.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, NgZone } from "@angular/core";
+import { ModalController, NavController, Platform } from 'ionic-angular';
+import { BookService } from '../../services/book.service';
+import { DetailsPage } from '../details/details';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public books = [];
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private bookService: BookService,
+              private nav: NavController,
+              private platform: Platform,
+              private zone: NgZone,
+              private modalCtrl: ModalController) {
   }
 
+  ionViewDidLoad() {
+    this.platform.ready().then(() => {
+      this.bookService.initDB();
+
+      this.bookService.getAll()
+        .then(data => {
+          this.zone.run(() => {
+            this.books = data;
+          });
+        })
+        .catch(console.error.bind(console));
+    });
+  }
+
+  showDetail(book) {
+    let modal = this.modalCtrl.create(DetailsPage, { book: book });
+    modal.present();
+  }
 }
